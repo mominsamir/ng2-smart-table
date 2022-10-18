a79e5f379dcef61e9e613c94698b3fd3e6c0831e
katebatura <katebatura@gmail.com>
2022-10-18 12:56:34 +0200

    bug/setting columnsSizeMap

--- projects/ngx-smart-table/src/lib/components/thead/rows/thead-filters-row.component.ts
+++ projects/ngx-smart-table/src/lib/components/thead/rows/thead-filters-row.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, Output, EventEmitter, OnChanges } from '@angular/core
 
 import { Grid } from '../../../lib/grid';
 import { DataSource } from '../../../lib/data-source/data-source';
-import { Column } from "../../../lib/data-set/column";
+import { Column } from '../../../lib/data-set/column';
 
 @Component({
 	selector: '[ng2-st-thead-filters-row]',
@@ -14,40 +14,28 @@ import { Column } from "../../../lib/data-set/column";
                           (create)="create.emit($event)">
     </th>
     <ng-container *ngFor="let column of getVisibleColumns(grid.getColumns())">
-		<th
-			*ngIf="isPrimaryColumn(column.id)  && rowCollapsEnabled()"
-			[ngClass]="{'ngx-fixed-header': grid.isTableTypePivot()}"
-			[style.left]="'calc(0px)'"
-			[style.zIndex]="11"
-			[style.minWidth]="rowCollapsWidth()">
-		</th>
-		<th
-			class="ng2-smart-th {{ column.id }}"
-			*ngIf="column.groupBy"
-			[style.minWidth]="column.width"
-            [style.width]="column.width"
-            [style.zIndex]="11"
-			[ngClass]="{'ngx-fixed-header': grid.isTableTypePivot()}"
-			[ngStyle]=" {'left': 'calc('+grid.getColumnSize(column.id)+')' }">
-			<ng2-smart-table-filter [source]="source"
-				[column]="column"
-				[inputClass]="filterInputClass"
-				(filter)="filter.emit($event)">
-			</ng2-smart-table-filter>
-		</th>
-
-		<th
-			class="ng2-smart-th {{ column.id }}"
-			*ngIf="!column.groupBy"
-			[style.minWidth]="column.width"
-            [style.width]="column.width"
-			[ngClass]="{'ngx-fixed-header': grid.isTableTypePivot()}">
-			<ng2-smart-table-filter [source]="source"
-				[column]="column"
-				[inputClass]="filterInputClass"
-				(filter)="filter.emit($event)">
-			</ng2-smart-table-filter>
-		</th>
+      <th
+        *ngIf="isPrimaryColumn(column.id)  && rowCollapsEnabled()"
+        [class.ngx-fixed-header]="grid.isTableTypePivot()"
+        [style.left]="'0px'"
+        [style.zIndex]="11"
+        [style.width]="rowCollapsWidth()"
+        [style.minWidth]="rowCollapsWidth()">
+      </th>
+      <th
+        class="ng2-smart-th {{ column.id }}"
+        [class.ngx-fixed-header]="grid.isTableTypePivot()"
+        [style.minWidth]="column.width"
+        [style.width]="column.width"
+        [style.left]="column.groupBy && 'calc('+grid.getColumnSize(column.id)+')'"
+        [style.zIndex]="column.groupBy && 11">
+        <ng2-smart-table-filter
+          [source]="source"
+          [column]="column"
+          [inputClass]="filterInputClass"
+          (filter)="filter.emit($event)">
+        </ng2-smart-table-filter>
+      </th>
     </ng-container>
     <th ng2-st-add-button *ngIf="showActionColumnRight  && !grid.isTableTypePivot()"
                           [grid]="grid"
